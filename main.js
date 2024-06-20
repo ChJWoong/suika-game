055e9eb78ca2fbd31983b160ea76bab6ebabc66f
최지웅 <wkdms7890@naver.com>
2024-06-21 00:43:20 +0900

    Update main.js

--- main.js
+++ main.js
@@ -585,6 +585,7 @@ function getAllObjectPositions() {
 
 const times = [];
 let tempFPS = 0;
+let gameOver = false;
 
 //충돌 체크용
 Matter.Events.on(engine, "beforeUpdate", checkForGameOver);
@@ -612,9 +613,13 @@ function getBodiesAboveTopLine() {
 
 // 게임 오버 체크 함수
 function checkForGameOver() {
+  if (gameOver) {
+    return;
+  }
   const bodiesAboveTopLine = getBodiesAboveTopLine();
 
   if (bodiesAboveTopLine.length > 0) {
+    gameOver = true;
     alert("게임 오버");
     location.reload(true);
   }
